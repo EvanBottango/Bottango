1a1326f75d57ba07b25949be043e82d45739cfdb
Evan McMahon <evan@bottango.com>
2026-06-17 09:51:28 -0700

    Remove unused FreeRam helper

--- BottangoArduinoDriver/src/FreeRam.cpp
+++
@@ -1,9 +0,0 @@
-
-void printFreeRam()
-{
-#ifdef BOTTANGO_DEBUG
-    extern int __heap_start, *__brkval;
-    int v;
-    int freeRam = (int)&v - (__brkval == 0 ? (int)&__heap_start : (int)__brkval);
-#endif
-}
