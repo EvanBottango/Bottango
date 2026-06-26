7d722368ae5a0fbf22ee2f38239d1d55b6b09b2f
Dschadu <darki.klein@gmail.com>
2026-06-26 14:18:53 +0200

    Private Var Naming, Documentation and const
    Changed private vars to m_* naming pattern
    Added Documentation
    Added const keyword to some functions

--- BottangoArduinoDriver/src/System/Time.h
+++ BottangoArduinoDriver/src/System/Time.h
@@ -5,14 +5,33 @@
 
 namespace Time
 {
+	/**
+	 * @brief Synchronizes the internal time with an incoming time value. This function updates the last synchronized time and calculates the time offset based on the current millis() value.
+	 * @param incomingTime The time value to synchronize with, in milliseconds.
+	 */
 	void syncTime(unsigned long incomingTime);
 
+	/**
+	 * @brief Returns the current time in milliseconds, adjusted by the time offset. If time is stopped (in relay-supported mode), it returns 0.
+	 * @return The current time in milliseconds, adjusted by the time offset.
+	 */
 	unsigned long getCurrentTimeInMs();
 
+	/**
+	 * @brief Returns the last time value that was passed to syncTime().
+	 * @return The last incomingTime value that was passed to syncTime(), in milliseconds.
+	 */
 	unsigned long getLastSyncedTimeInMs();
 
 #ifdef RELAY_SUPPORTED
+	/**
+	 * @brief Stops the time tracking, effectively freezing the current time. This function is only available in relay-supported mode.
+	 */
 	void stopTime();
+
+	/**
+	 * @brief Indicates whether the time tracking is currently stopped. This variable is only available in relay-supported mode.
+	 */
 	extern bool timeStopped;
 #endif
 
--- BottangoArduinoDriver/src/Util/CircularArray.h
+++ BottangoArduinoDriver/src/Util/CircularArray.h
@@ -3,50 +3,87 @@
 #include <Arduino.h>
 #include "../System/Errors.h"
 
+/**
+ * @brief A circular array implementation that allows for efficient storage and retrieval of elements in a fixed-size buffer.
+ * @tparam T The type of elements stored in the circular array.
+ */
 template <class T>
 class CircularArray
 {
-	T** array;
-	byte capacity;
-	byte idx;
-
 public:
+	/**
+	 * @brief Constructor for the CircularArray class. Initializes the array with a specified capacity and sets the index to 0.
+	 * @tparam T The type of elements stored in the circular array.
+	 * @param capacity The maximum number of elements the circular array can hold.
+	 */
 	explicit CircularArray(int capacity);
 
+	/**
+	 * @brief Inserts a new element at the end of the circular array. If the array is full, an error is reported and the element is not added.
+	 * @tparam T The type of elements stored in the circular array.
+	 * @param data The element to be added to the circular array.
+	 */
 	void pushBack(T* data);
 
+	/**
+	 * @brief Removes a specific element from the circular array. If the element is found, it is removed and the subsequent elements are shifted to fill the gap.
+	 * @note Does not delete the element itself, only removes it from the array.
+	 * @tparam T The type of elements stored in the circular array.
+	 * @param toRemove  The element to be removed from the circular array.
+	 */
 	void remove(T* toRemove);
 
-	T* get(int index);
+	/**
+	 * @brief Helper function to retrieve an element at a specific index in the circular array. Returns a pointer to the element if it exists, or NULL if the index is out of bounds.
+	 * @tparam T The type of elements stored in the circular array.
+	 * @param index The index of the element to retrieve. Must be less than the current size of the array.
+	 * @return The element at the specified index, or NULL if the index is invalid.
+	 */
+	T* get(int index) const;
 
-	byte size();
+	/**
+	 * @brief Helper function to get the current number of elements in the circular array. Returns the number of elements currently stored in the array.
+	 * @tparam T The type of elements stored in the circular array.
+	 * @return The number of elements currently stored in the circular array.
+	 */
+	byte size() const;
 
+	/**
+	 * @brief Clears the circular array by setting all elements to NULL and resetting the index to 0. This effectively removes all elements from the array without deallocating the memory.
+	 * @note Does not delete the elements themselves, only clears the references in the array.
+	 * @tparam T The type of elements stored in the circular array.
+	 */
 	void clear();
-};
+
+private:
+	T** m_array;
+	byte m_capacity;
+	byte m_idx;
+}; 
 
 template <class T>
-CircularArray<T>::CircularArray(int _capacity)
+CircularArray<T>::CircularArray(int capacity)
 {
-	capacity = _capacity;
-	array = new T * [capacity];
-	idx = 0;
+	m_capacity = capacity;
+	m_array = new T * [m_capacity];
+	m_idx = 0;
 
-	for (int i = 0; i < capacity; ++i)
+	for (int i = 0; i < m_capacity; ++i)
 	{
-		array[i] = NULL;
+		m_array[i] = NULL;
 	}
 }
 
 template <class T>
 void CircularArray<T>::pushBack(T* data)
 {
-	if (idx >= capacity)
+	if (m_idx >= m_capacity)
 	{
 		Error::reportError_NoSpaceAvailable();
 		return;
 	}
 
-	array[idx++] = data;
+	m_array[m_idx++] = data;
 }
 
 template <class T>
@@ -54,9 +91,9 @@ void CircularArray<T>::remove(T* toRemove)
 {
 	// find index of element to remove
 	int index = -1;
-	for (int i = 0; i < capacity; i++)
+	for (int i = 0; i < m_capacity; i++)
 	{
-		if (array[i] == toRemove)
+		if (m_array[i] == toRemove)
 		{
 			index = i;
 			break;
@@ -70,43 +107,43 @@ void CircularArray<T>::remove(T* toRemove)
 	}
 
 	// Move everything ahead of the found index back one index
-	for (int i = index; i < capacity - 1; i++)
+	for (int i = index; i < m_capacity - 1; i++)
 	{
-		array[i] = array[i + 1];
+		m_array[i] = m_array[i + 1];
 	}
 
 	// if next add would add past max, move next add back to end
-	if (idx >= capacity)
+	if (m_idx >= m_capacity)
 	{
-		idx = capacity - 1;
-		array[idx] = NULL;
+		m_idx = m_capacity - 1;
+		m_array[m_idx] = NULL;
 	}
 	// otherwise just clear at wherever idx is and move idx down
 	else
 	{
-		array[idx] = NULL;
-		idx--;
+		m_array[m_idx] = NULL;
+		m_idx--;
 	}
 }
 
 template <class T>
-T* CircularArray<T>::get(int index)
+T* CircularArray<T>::get(int index) const
 {
-	return array[index];
+	return m_array[index];
 }
 
 template <class T>
-byte CircularArray<T>::size()
+byte CircularArray<T>::size() const
 {
-	return idx;
+	return m_idx;
 }
 
 template <class T>
 void CircularArray<T>::clear()
 {
-	for (int i = 0; i < capacity; ++i)
+	for (int i = 0; i < m_capacity; ++i)
 	{
-		array[i] = NULL;
+		m_array[i] = NULL;
 	}
-	idx = 0;
+	m_idx = 0;
 }
